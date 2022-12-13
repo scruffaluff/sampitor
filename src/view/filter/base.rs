597e12e4c6b6a93922f7193dc6757459bcef445b
Macklan Weinstein <macklan.weinstein@gmail.com>
2022-12-12 21:55:33 -0800

    :sparkles: Add blanket reference trait implementations

--- src/view/filter/base.rs
+++ src/view/filter/base.rs
@@ -8,7 +8,33 @@ pub trait Knob {
     fn text(&self) -> &str;
 }
 
+/// Blanket ergonomic mplementation for &mut K.
+impl<K: Knob> Knob for &mut K {
+    fn decrement(&mut self) {
+        K::decrement(self);
+    }
+
+    fn increment(&mut self) {
+        K::increment(self);
+    }
+
+    fn text(&self) -> &str {
+        K::text(self)
+    }
+}
+
 pub trait Filter {
     fn knobs(&mut self) -> Vec<(&str, &mut dyn Knob)>;
     fn process(&mut self, samples: &mut Samples);
+}
+
+/// Blanket ergonomic mplementation for &mut F.
+impl<F: Filter> Filter for &mut F {
+    fn knobs(&mut self) -> Vec<(&str, &mut dyn Knob)> {
+        F::knobs(self)
+    }
+
+    fn process(&mut self, samples: &mut Samples) {
+        F::process(self, samples);
+    }
 }
